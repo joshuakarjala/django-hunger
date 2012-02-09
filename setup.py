d541a8fe323aabd8293321071807e2bfa9b79cfa
Joshua Karjala <joshua@parentes.org>
2012-02-09 02:23:42 -0800

    Pypi uploading stuf

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import hunger
 DESCRIPTION = "A Django app to mange a private beta phase."
 
 try:
-    LONG_DESCRIPTION = open('README.rst').read()
+    LONG_DESCRIPTION = open('README').read()
 except:
     pass
 
