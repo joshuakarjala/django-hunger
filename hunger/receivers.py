8285fc80fb20457c528095c4a8ee17136f5520eb
Simon Ye <sye737@gmail.com>
2012-02-28 00:44:17 -0800

    Reasonable defaults for email sending.

--- hunger/receivers.py
+++ hunger/receivers.py
@@ -1,36 +1,44 @@
 import datetime
 import importlib
-from django.conf import settings
 from hunger.models import InvitationCode
+from hunger.utils import setting
 
-#send confirmation email to user
+
 def invitation_code_created(sender, email, **kwargs):
-    email_module = importlib.import_module(settings.BETA_EMAIL_MODULE)
-    email_function = getattr(email_module, settings.BETA_EMAIL_CONFIRM_FUNCTION)
+    """Send confirmation email to user."""
+    email_module_name = setting('BETA_EMAIL_MODULE', 'hunger.email')
+    email_module = importlib.import_module(email_module_name)
+    email_function_name = setting('BETA_EMAIL_CONFIRM_FUNCTION', 'beta_confirm')
+    email_function = getattr(email_module, email_function_name)
     email_function(email, **kwargs)
 
-#send invitation code to user
+
 def invitation_code_sent(sender, email, invitation_code, **kwargs):
+    """Send invitation code to user."""
     try:
         invitation_code = InvitationCode.objects.get(email=email)
-        invitation_code.is_invited = True
-        invitation_code.invited = datetime.datetime.now()
-        invitation_code.save()
-
-        email_module = importlib.import_module(settings.BETA_EMAIL_MODULE)
-        email_function = getattr(email_module, settings.BETA_EMAIL_INVITE_FUNCTION)
-        email_function(email, invitation_code.code, **kwargs)
-
     except InvitationCode.DoesNotExist:
-        pass
+        return
 
-#when user with corresponding email has been created then set the code as used.
+    invitation_code.is_invited = True
+    invitation_code.invited = datetime.datetime.now()
+    invitation_code.save()
+
+    email_module_name = setting('BETA_EMAIL_MODULE', 'hunger.email')
+    email_module = importlib.import_module(email_module_name)
+    email_function_name = setting('BETA_EMAIL_INVITE_FUNCTION', 'beta_invite')
+    email_function = getattr(email_module, email_function_name)
+    email_function(email, invitation_code.code, **kwargs)
+
+
 def invitation_code_used(sender, user, invitation_code, **kwargs):
+    """Set the invitation code as used by user."""
     try:
         invitation_code = InvitationCode.objects.get(code=invitation_code)
-        invitation_code.user = user
-        invitation_code.is_used = True
-        invitation_code.used = datetime.datetime.now()
-        invitation_code.save()
     except InvitationCode.DoesNotExist:
-        pass
+        return
+
+    invitation_code.user = user
+    invitation_code.is_used = True
+    invitation_code.used = datetime.datetime.now()
+    invitation_code.save()
