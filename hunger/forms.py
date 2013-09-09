6de6385f07d9b94575e025a3ab956d669ae9b9a1
Zenobius Jiricek <zenobius.jiricek@gmail.com>
2013-09-09 10:33:49 +0930

    Update forms.py

--- hunger/forms.py
+++ hunger/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from hunger.models import Invitation
+from hunger.models import Invitation, InvitationCode
+
 
 class InviteSendForm(forms.ModelForm):
     class Meta:
         model = Invitation
         fields = ('email',)
+
+
+class InvitationCodeAdminForm(forms.ModelForm):
+    code = forms.CharField(initial=lambda: InvitationCode.generate_invite_code())
+    class Meta:
+        model = InvitationCode
+
